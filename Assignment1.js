71eed59443da6273e49fbec726d766671dc8be88
Showunmi cornelius Toyosi <117469004+Toyocasual@users.noreply.github.com>
2023-08-15 22:35:27 +0100

    Create Assignment1.js
    Create a function that takes two numbers and returns their sum and product.

---
+++ Assignment1.js
@@ -0,0 +1,25 @@
+//create a function that takes two numbers and returns their sum and product.
+
+
+
+/* function numbers(a,b){
+    let sum = a + b;
+        product = a * b;
+       return sum & product;
+     
+}
+
+let known = numbers(1,2);
+ console.log(known);
+ console.log(sum);
+ */
+
+ let a = 100;
+    let b = 200;
+ function numbers(){
+    
+    console.log(a + b);
+    console.log(a * b)
+
+ };
+ numbers();
