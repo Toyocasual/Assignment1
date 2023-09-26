d87f8d2e23850088a0afb0d73bffb101ccce4579
Showunmi cornelius Toyosi <117469004+Toyocasual@users.noreply.github.com>
2023-09-27 00:30:22 +0100

    Sum and product
    Create a function that takes two numbers and return their sum and product

--- Assignment1.js
+++ Assignment1.js
@@ -2,17 +2,7 @@
 
 
 
-/* function numbers(a,b){
-    let sum = a + b;
-        product = a * b;
-       return sum & product;
-     
-}
-
-let known = numbers(1,2);
- console.log(known);
- console.log(sum);
- */
+ 
 
  let a = 100;
     let b = 200;
